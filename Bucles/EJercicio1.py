657262da2b4d42941a439178e0213ee14bd7753b
AriadnaLZ <ariadnalopezzarate@gmail.com>
2024-04-24 10:56:41 +0200

    He creado una carpeta Bucles

---
+++ Bucles/EJercicio1.py
@@ -0,0 +1,4 @@
+# 1. Ejercicio: Imprime los números del 1 al 10 usando un bucle for .
+
+for num in range(1, 11):
+  print(num)
---
+++ Bucles/Ejercicio3.py
@@ -0,0 +1,9 @@
+# 3. Ejercicio: Usa un bucle para calcular la suma de los números del 1 al 100.
+
+suma = 0
+i  = 1
+
+while i <= 100:
+  suma +=i
+  i +=1
+  print(suma)
